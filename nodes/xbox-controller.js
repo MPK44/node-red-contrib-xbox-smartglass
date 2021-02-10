63f35db6af4d2aa00eacbcafe86eb25e3aa72308
MPK44 <MPK44@users.noreply.github.com>
2021-02-10 18:12:54 +0100

    added Poweron/off & getActiveApp
    msg.payload supports on/off/getActiveApp now

--- nodes/xbox-controller.js
+++ nodes/xbox-controller.js
@@ -1,48 +1,81 @@
 module.exports = function (RED) {
-    let sgClient = require('xbox-smartglass-core-node/src/smartglass')();
-    let SystemInputChannel = require('xbox-smartglass-core-node/src/channels/systeminput');
+  let sgClient = require('xbox-smartglass-core-node/src/smartglass')();
+  let SystemInputChannel = require('xbox-smartglass-core-node/src/channels/systeminput');
 
-    function connect() {
-        return new Promise(function (myResolve, myReject) {
-            if (sgClient.isConnected()) {
-                myResolve()
-            } else {
-                sgClient.connect().then(function () {
-                    sgClient.addManager('system_input', SystemInputChannel());
-                    setTimeout(myResolve, 1000)
-                }, function (error) {
-                    myReject(error)
-                });
-            }
+  function connect() {
+    return new Promise(function (myResolve, myReject) {
+      if (sgClient.isConnected()) {
+        myResolve();
+      } else {
+        sgClient.connect().then(function () {
+          sgClient.addManager('system_input', SystemInputChannel());
+          setTimeout(myResolve, 1000);
+        }, function (error) {
+          myReject(error.message);
         });
-    }
+      }
+    });
+  }
 
-    function XboxControllerNode(config) {
-        RED.nodes.createNode(this, config);
-        var node = this;
+  function XboxControllerNode(config) {
+    RED.nodes.createNode(this, config);
+    var node = this;
 
-        node.on('input', msg => {
-            if (["a", "b", "x", "y", "up", "left", "right", "down", "nexus", "view", "menu"].indexOf(msg.payload) == -1) {
-                node.error("Unsupported input: " + msg.payload);
-                return
-            }
-            connect().then(function () {
-                sgClient.getManager('system_input').sendCommand(msg.payload).then(function (button) {
-                    node.send(msg)
-                }, function (error) {
-                    node.error(error);
-                    console.log(error)
-                });
-            }, function (error) {
-                node.error(error);
-                console.log(error)
-            });
+    node.on('input', msg => {
+      if (["a", "b", "x", "y", "up", "left", "right", "down", "nexus", "view", "menu"].indexOf(msg.payload) != -1) {
+        connect().then(function () {
+          sgClient.getManager('system_input').sendCommand(msg.payload).then(function (button) {
+            node.send(msg);
+          }, function (error) {
+            node.error(error);
+            console.log(error);
+          });
+        }, function (error) {
+          node.error(error);
+          console.log(error);
         });
+      } else if (msg.payload == 'on') {
+        var options = {
+          'ip': '',
+          'live_id': ''
+        };
 
-        this.on('close', done => {
-            sgClient.disconnect()
-            done()
+        sgClient.powerOn(options).then(function () {
+          node.send(msg);
+        }, function (error) {
+          node.error(error.error);
+          console.log(error.error);
         });
-    }
-    RED.nodes.registerType("Xbox-controller", XboxControllerNode);
+      } else if (msg.payload == 'off') {
+        connect().then(function () {
+          sgClient.powerOff().then(function () {
+            node.send(msg);
+          }, function (error) {
+            node.error(error);
+            console.log(error);
+          });
+        }, function (error) {
+          node.error(error);
+          console.log(error);
+        });
+      } else if (msg.payload == "getActiveApp") {
+        connect().then(function () {
+          msg.app = sgClient.getActiveApp();
+          node.send(msg);
+        }, function (error) {
+          node.error(error);
+          console.log(error);
+        });
+      } else {
+        node.error("Unsupported input: " + msg.payload);
+      };
+
+    });
+
+    this.on('close', done => {
+      sgClient.disconnect()
+      done()
+    });
+  }
+  RED.nodes.registerType("Xbox-controller", XboxControllerNode);
 }
